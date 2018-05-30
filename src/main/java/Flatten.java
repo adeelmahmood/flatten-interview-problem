e8b1782e6bd0ff4795396913e33a90345e5e078a
Adeel Qureshi <aqureshi@mobitv.com>
2018-05-30 11:42:39 -0700

    added json

--- src/main/java/Flatten.java
+++ src/main/java/Flatten.java
@@ -1,8 +1,29 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public class Flatten {
 
     public static void main(String[] args) throws IOException {
+
+        String json = "[\n" +
+                "{\"a\":\n" +
+                "  {\"b\":\n" +
+                "    {\"c\":\n" +
+                "      {\"d\":\"e\", \"d1\":{\"f\":\"g\"}}\n" +
+                "    }\n" +
+                "  },\n" +
+                "\"a1\":\"b\"\n" +
+                "},\n" +
+                "{\"a\":{\"b\":{\"c\":{\"d\":{\"e\":\"f\"}}}}}\n" +
+                "]";
+
+        ObjectMapper m = new ObjectMapper();
+        List<Map<String, Object>> data = m.readValue(json, List.class);
+
+        System.out.println(data);
     }
 }
 
